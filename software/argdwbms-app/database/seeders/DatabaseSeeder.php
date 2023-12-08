cb605bdece6e00e3c2af9fccf8d4a9b5015da886
Marvin M. Ramos <115031416+Crispkreme@users.noreply.github.com>
2023-12-08 10:00:08 -0800

    full auth

--- software/argdwbms-app/database/seeders/DatabaseSeeder.php
+++ software/argdwbms-app/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Sensor;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,18 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        for ($i = 0; $i < 1000; $i++) {
+            Sensor::create([
+                'temperature' => rand(0, 1000),
+                'humidity' => rand(0, 1000),
+            ]);
+        }
+
+        User::create([
+            'name' => "Marvin Ramos",
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin@0896'),
+        ]);
     }
 }
--- software/argdwbms-app/resources/views/auth/login.blade.php
+++ software/argdwbms-app/resources/views/auth/login.blade.php
@@ -1,73 +1,148 @@
-@extends('layouts.app')
+{{-- <x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
-    </div>
-</div>
-@endsection
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            </label>
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+
+            <x-primary-button class="ms-3">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout> --}}
+
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="csrf-token" content="{{ csrf_token() }}">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="../images/favicon.ico">
+
+		<title>{{ config('app.name', 'Laravel') }}</title>
+
+		<!-- Vendors Style-->
+		<link rel="stylesheet" href="{{ asset('css/vendors_css.css') }}">
+
+		<!-- Style-->
+		<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+		<link rel="stylesheet" href="{{ asset('css/skin_color.css') }}">
+	</head>
+	<body class="dark-skin sidebar-mini theme-primary fixed sidebar-mini-expand-feature" style="height: auto; min-height: 100%;">
+
+	<div class="container h-p100">
+		<div class="row align-items-center justify-content-md-center h-p100">
+
+			<div class="col-12">
+				<div class="row justify-content-center no-gutters">
+					<div class="col-lg-4 col-md-5 col-12">
+						<div class="content-top-agile p-10">
+							<h2 class="text-white">Get started with Us</h2>
+							<p class="text-white-50">Sign in to start your session</p>
+						</div>
+						<div class="p-30 rounded30 box-shadowed b-2 b-dashed">
+
+							<form method="POST" action="{{ route('login') }}">
+							@csrf
+								<div class="form-group">
+									<div class="input-group mb-3">
+										<div class="input-group-prepend">
+											<span class="input-group-text bg-transparent text-white"><i class="ti-user"></i></span>
+										</div>
+										<input type="text" name="email" class="form-control pl-15 bg-transparent text-white plc-white" required autofocus autocomplete="email">
+									</div>
+								</div>
+								<div class="form-group">
+									<div class="input-group mb-3">
+										<div class="input-group-prepend">
+											<span class="input-group-text  bg-transparent text-white"><i class="ti-lock"></i></span>
+										</div>
+										<input type="password" name="password" class="form-control pl-15 bg-transparent text-white plc-white" required autofocus autocomplete="password">
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-6">
+									<div class="checkbox text-white">
+										<input type="checkbox" id="basic_checkbox_1" name="remember">
+										<label for="basic_checkbox_1">Remember Me</label>
+									</div>
+									</div>
+									<!-- /.col -->
+									<div class="col-6">
+										<div class="fog-pwd text-right">
+											@if (Route::has('password.request'))
+												<a href="javascript:void(0)" class="text-white hover-info" href="{{ route('password.request') }}">
+													<i class="ion ion-locked"></i>
+													Forgot pwd?
+												</a>
+												<br>
+											@endif
+										</div>
+									</div>
+									<!-- /.col -->
+									<div class="col-12 text-center">
+										<button type="submit" class="btn btn-info btn-rounded mt-10">SIGN IN</button>
+									</div>
+									<!-- /.col -->
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<!-- Vendor JS -->
+	<script src="{{ asset('js/vendors.min.js') }}"></script>
+	<script src="{{ asset('assets/icons/feather-icons/feather.min.js') }}"></script>
+	<script src="{{ asset('assets/vendor_components/easypiechart/dist/jquery.easypiechart.js') }}"></script>
+	<script src="{{ asset('assets/vendor_components/apexcharts-bundle/irregular-data-series.js') }}"></script>
+	<script src="{{ asset('assets/vendor_components/apexcharts-bundle/dist/apexcharts.js') }}"></script>
+
+	<!-- Sunny Admin App -->
+	<script src="{{ asset('js/template.js') }}"></script>
+	<script src="{{ asset('js/pages/dashboard.js') }}"></script>
+
+	</body>
+</html>
